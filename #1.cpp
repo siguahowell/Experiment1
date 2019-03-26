e58e96e3dc4eefe09e5a643356f310b57307d895
siguahowell <47727452+siguahowell@users.noreply.github.com>
2019-03-26 22:34:52 +0800

    Update #1.cpp

--- #1.cpp
+++ #1.cpp
@@ -6,19 +6,30 @@ using namespace std;
 
     int main()
     {
+	int box = 250;
+	int sideline = 100;
+	int premium = 50;
+	int general = 25;
+	int a= 5750, b = 28000, c=35750, d=18750;    
     // variables to be inserted
     	int ticketprice;
     	int numberOfTicketsSold;
     	int total;
     // input the price of ticket
-	cout << "input the price of ticket that was sold \n";
-	cin >> ticketprice;
-	//input the number of people whou bought the ticket
-	cout<< "input the number of people who bought the ticket \n";
-	cin>> numberOfTicketsSold;
-	total=ticketprice*numberOfTicketsSold;
+    cout<< "------------------------------------------ \n";
+	cout << "in box tickets the total sales is"<< box*a <<"$"<<endl;
 	cout<< "------------------------------------------ \n";
-	cout << "The total price is:"<<total<<"$"<<endl;
+	//input the number of people whou bought the ticket
+	cout<< "in sideline the total sales is"<<sideline*b<<"$"<<endl;
+	cout<< "------------------------------------------ \n";
+	cout << "in premium the total sales is "<<premium*c<<"$"<<endl;
+	cout<< "------------------------------------------ \n";
+	cout << "in general admission the total sales is"<<d*general<<"$"<<endl;
+	cout<< "------------------------------------------ \n";
+	total=( box*a)+(sideline*b)+(premium*c)+(d*general);
+	cout<< "------------------------------------------ \n";
+	cout << "The total price is:"<< total<<"$"<<endl;
+	cout<< "------------------------------------------ \n";
         getch();
         return 0;
     }
